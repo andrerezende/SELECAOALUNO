61fe675cacedc16beaca216e59f7a4dd004d9cf3
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2011-06-06 10:35:34 -0300

    script do banco de dados incluído

--- administracao/db/dump_db.sql
+++ administracao/db/dump_db.sql
@@ -1,58 +1,47 @@
--- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (i486)
---
--- Host: localhost    Database: dump_selecao
--- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12.10
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+SET AUTOCOMMIT=0;
+START TRANSACTION;
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `campus`
+-- Estrutura da tabela `campus`
 --
 
-DROP TABLE IF EXISTS `campus`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `campus` (
   `id` int(11) NOT NULL,
   `nome` varchar(128) DEFAULT NULL,
   `ativo` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `curso`
+-- Estrutura da tabela `curso`
 --
 
-DROP TABLE IF EXISTS `curso`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `curso` (
   `cod_curso` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(128) NOT NULL,
   `ativo` char(50) DEFAULT NULL,
   `campus` int(11) DEFAULT NULL,
   PRIMARY KEY (`cod_curso`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `inscrito`
+-- Estrutura da tabela `inscrito`
 --
 
-DROP TABLE IF EXISTS `inscrito`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `inscrito` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(128) NOT NULL,
@@ -93,81 +82,68 @@ CREATE TABLE `inscrito` (
   `data_cadastro` datetime DEFAULT NULL,
   `ultima_alteracao` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `inscrito_curso`
+-- Estrutura da tabela `inscrito_curso`
 --
 
-DROP TABLE IF EXISTS `inscrito_curso`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `inscrito_curso` (
   `id_inscrito` int(11) NOT NULL,
   `cod_curso` int(11) NOT NULL,
   PRIMARY KEY (`id_inscrito`,`cod_curso`),
   KEY `cod_curso` (`cod_curso`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `localprova`
+-- Estrutura da tabela `localprova`
 --
 
-DROP TABLE IF EXISTS `localprova`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `localprova` (
   `id` int(11) NOT NULL,
   `nome` varchar(128) DEFAULT NULL,
   `ativo` varchar(45) DEFAULT NULL,
   `campus` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `pagamentos`
+-- Estrutura da tabela `pagamentos`
 --
 
-DROP TABLE IF EXISTS `pagamentos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pagamentos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_inscrito` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
-  `arqretorno` varchar(45) COLLATE latin1_general_ci DEFAULT NULL,
+  `id_inscrito` varchar(45) DEFAULT NULL,
+  `arqretorno` varchar(45) DEFAULT NULL,
   `datapagamento` date DEFAULT NULL,
   `dataretorno` date DEFAULT NULL,
   `dataimportacao` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ID_INSCRITO` (`id_inscrito`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estrutura da tabela `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `usuario` (
   `usuario` varchar(50) NOT NULL,
   `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- --------------------------------------------------------
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2011-06-02 12:21:28
