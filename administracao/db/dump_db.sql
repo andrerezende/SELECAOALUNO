8f7ba4396faff2cdc8b39ac013a0f9e560d9a99c
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2011-06-07 11:42:39 -0300

    script do banco atualizado

--- administracao/db/dump_db.sql
+++ administracao/db/dump_db.sql
@@ -1,8 +1,5 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-SET AUTOCOMMIT=0;
-START TRANSACTION;
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,12 +12,12 @@ START TRANSACTION;
 -- Estrutura da tabela `campus`
 --
 
-CREATE TABLE `campus` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `campus` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(128) DEFAULT NULL,
-  `ativo` varchar(45) DEFAULT NULL,
+  `ativo` tinyint(4) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -28,13 +25,14 @@ CREATE TABLE `campus` (
 -- Estrutura da tabela `curso`
 --
 
-CREATE TABLE `curso` (
+CREATE TABLE IF NOT EXISTS `curso` (
   `cod_curso` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(128) NOT NULL,
-  `ativo` char(50) DEFAULT NULL,
+  `nome` varchar(255) NOT NULL,
+  `ativo` tinyint(4) DEFAULT '1',
   `campus` int(11) DEFAULT NULL,
-  PRIMARY KEY (`cod_curso`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`cod_curso`),
+  KEY `fk_curso_campus` (`campus`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=73 ;
 
 -- --------------------------------------------------------
 
@@ -42,7 +40,7 @@ CREATE TABLE `curso` (
 -- Estrutura da tabela `inscrito`
 --
 
-CREATE TABLE `inscrito` (
+CREATE TABLE IF NOT EXISTS `inscrito` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(128) NOT NULL,
   `endereco` varchar(128) NOT NULL,
@@ -69,7 +67,7 @@ CREATE TABLE `inscrito` (
   `responsavel` varchar(128) DEFAULT NULL,
   `isencao` varchar(45) DEFAULT NULL,
   `declaracao` varchar(128) DEFAULT NULL,
-  `localprova` varchar(128) DEFAULT NULL,
+  `localprova` int(11) DEFAULT NULL,
   `numinscricao` varchar(45) NOT NULL,
   `pagamento` varchar(45) DEFAULT NULL,
   `especial_prova` varchar(10) DEFAULT NULL,
@@ -77,12 +75,16 @@ CREATE TABLE `inscrito` (
   `vaga_especial` varchar(10) DEFAULT NULL,
   `vaga_rede_publica` varchar(10) DEFAULT NULL,
   `vaga_rural` varchar(10) DEFAULT NULL,
-  `campus` varchar(128) DEFAULT NULL,
+  `campus` int(11) DEFAULT NULL,
   `cadastro_unico` varchar(11) DEFAULT NULL,
   `data_cadastro` datetime DEFAULT NULL,
   `ultima_alteracao` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+  `curso` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_inscrito_curso` (`curso`),
+  KEY `fk_inscrito_campus` (`campus`),
+  KEY `fk_inscrito_localprova` (`localprova`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2292 ;
 
 -- --------------------------------------------------------
 
@@ -90,11 +92,10 @@ CREATE TABLE `inscrito` (
 -- Estrutura da tabela `inscrito_curso`
 --
 
-CREATE TABLE `inscrito_curso` (
+CREATE TABLE IF NOT EXISTS `inscrito_curso` (
   `id_inscrito` int(11) NOT NULL,
   `cod_curso` int(11) NOT NULL,
-  PRIMARY KEY (`id_inscrito`,`cod_curso`),
-  KEY `cod_curso` (`cod_curso`)
+  PRIMARY KEY (`id_inscrito`,`cod_curso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -103,13 +104,14 @@ CREATE TABLE `inscrito_curso` (
 -- Estrutura da tabela `localprova`
 --
 
-CREATE TABLE `localprova` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(128) DEFAULT NULL,
-  `ativo` varchar(45) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `localprova` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `ativo` tinyint(4) DEFAULT '1',
   `campus` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `fk_localprova_campus` (`campus`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -117,7 +119,7 @@ CREATE TABLE `localprova` (
 -- Estrutura da tabela `pagamentos`
 --
 
-CREATE TABLE `pagamentos` (
+CREATE TABLE IF NOT EXISTS `pagamentos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_inscrito` varchar(45) DEFAULT NULL,
   `arqretorno` varchar(45) DEFAULT NULL,
@@ -126,7 +128,7 @@ CREATE TABLE `pagamentos` (
   `dataimportacao` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ID_INSCRITO` (`id_inscrito`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=718 ;
 
 -- --------------------------------------------------------
 
@@ -134,16 +136,31 @@ CREATE TABLE `pagamentos` (
 -- Estrutura da tabela `usuario`
 --
 
-CREATE TABLE `usuario` (
+CREATE TABLE IF NOT EXISTS `usuario` (
   `usuario` varchar(50) NOT NULL,
   `senha` varchar(50) NOT NULL,
   PRIMARY KEY (`usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+--
+-- Restrições para as tabelas dumpadas
+--
 
-COMMIT;
+--
+-- Restrições para a tabela `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `fk_curso_campus` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Restrições para a tabela `inscrito`
+--
+ALTER TABLE `inscrito`
+  ADD CONSTRAINT `fk_inscrito_campus` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_inscrito_localprova` FOREIGN KEY (`localprova`) REFERENCES `localprova` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Restrições para a tabela `localprova`
+--
+ALTER TABLE `localprova`
+  ADD CONSTRAINT `fk_localprova_campus` FOREIGN KEY (`campus`) REFERENCES `campus` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
